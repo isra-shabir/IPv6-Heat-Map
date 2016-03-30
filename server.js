c2cafb9c5b04056186b622f3c2ffbd9424cb45c4
Isra Shabir <isra.shabir@gmail.com>
2016-03-30 03:09:28 -0400

    added ajax for sending form data

--- server.js
+++ server.js
@@ -18,51 +18,64 @@ function handleError(res, reason, message, code) {
   res.status(code || 500).json({"error": message});
 }
 
+// parse the data, and store in memory
+var fs = require('fs');
+var parse = require('csv').parse; // server side module to parse a csv file.
+
+var totalCount; // total lat,longs
+var globalData; // our parsed data array
+var IPTable;  // our desired values (lat,long, & IPv6 of these lat,long)
+var densityHash; // count of IPv6 for each lat, long
+var densityList;
+
+var parser = parse(function(err, data){
+
+  totalCount = data.length;
+  globalData = data;
+
+  IPTable = []; // table: [[IpV6, [lat, long]], ... , [[IpV6],[lat,long]]]
+  densityHash = {}; // hash: {[lat,long] : count}
+  for (var i =0; i < globalData.length; i++){
+
+    IPTable.push([globalData[i][1], [globalData[i][7], globalData[i][8]]]);
+    latLong = [IPTable[i][1]];
+
+    if (densityHash[latLong]) {
+      densityHash[latLong] +=1;
+    }
+    else {
+      densityHash[latLong] = 1;
+  }
+  }
+  densityList = [];
+  Object.keys(densityHash).forEach(function(key){
+        var b = key.split(',').map(Number);
+        b.push(densityHash[key]/totalCount);
+        densityList.push(b);
+      });
+  // console.log(densityList);
+  // console.log(globalData);
+  });
+
+fs.createReadStream('file.csv').pipe(parser);
+
+
 /*
-*  "/"                                      
+*  "/"  endpoint                                    
 *  sends the data required to populate heatmap
 */
+app.get('/', function(req, res) {
 
- app.get('/', function(req, res) {
+  res.send(densityList);
 
-        var fs = require('fs');
-        var parse = require('csv').parse; // server side module to parse a csv file.
-
-        var totalCount; // total lat,longs
-        var globalData; // our parsed data array
-        var IPTable;  // our desired values (lat,long, & IPv6 of these lat,long)
-        var densityHash; // count of IPv6 for each lat, long
-        var densityList;
-
-        var parser = parse(function(err, data){
-
-          totalCount = data.length;
-          globalData = data;
-
-          IPTable = []; // table: [[IpV6, [lat, long]], ... , [[IpV6],[lat,long]]]
-          densityHash = {}; // hash: {[lat,long] : count}
-          for (var i =0; i < globalData.length; i++){
-
-            IPTable.push([globalData[i][1], [globalData[i][7], globalData[i][8]]]);
-            latLong = [IPTable[i][1]];
-
-            if (densityHash[latLong]) {
-              densityHash[latLong] +=1;
-            }
-            else {
-              densityHash[latLong] = 1;
-          }
-          }
-
-          densityList = [];
-          Object.keys(densityHash).forEach(function(key){
-                var b = key.split(',').map(Number);
-                b.push(densityHash[key]/totalCount);
-                densityList.push(b);
-              });
-          });
-
-        fs.createReadStream('file.csv').pipe(parser);
-
-        res.send(densityList); 
 });
+
+/*
+*  "/"  endpoint                                    
+*  sends the data required to populate heatmap
+*/
+app.get("/data", function(req, res) {
+  console.log(req.body);
+});
+
+

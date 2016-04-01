bdba51429791a889da2e4a84ca90f7865f4dfa8d
Isra Shabir <isra.shabir@gmail.com>
2016-04-01 16:08:10 -0400

    heat map works

--- server.js
+++ server.js
@@ -4,19 +4,8 @@ var bodyParser = require("body-parser");
 
 var app = express();
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 
-// Initialize the app.
-var server = app.listen(process.env.PORT || 5000, function () {
-  var port = server.address().port;
-  console.log("App now running on port", port);
-  });
-
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": message});
-}
 
 // parse the data, and store in memory
 var fs = require('fs');
@@ -56,7 +45,6 @@ var parser = parse(function(err, data){
   // console.log(densityList);
   // console.log(globalData);
   });
-
 fs.createReadStream('file.csv').pipe(parser);
 
 /*
@@ -64,26 +52,46 @@ fs.createReadStream('file.csv').pipe(parser);
 *  sends the data required to populate heatmap
 */
 app.get('/', function(req, res) {
-  res.send(densityList);
+  res.sendFile(__dirname + "/public/index.html");
+});
 
+/*
+*  "/heatmap" endpoint
+*  sends [[lat,long, intensity], ..., []].
+*  used by client to populate heatmap
+*/
+
+app.post('/heatmap', function(req, res) {
+  res.send(densityList);
 });
 
 /*
 *  "/geodata"  endpoint                                    
 *   uses the params sent from the client side
-*   sends response to client with list of coordinates
+*   sends response to client with list of interested
+*   coordinates
 */
 app.post("/geodata", function(req, res) {
 
-  // upon receiving the bounding box,
-  // execute:
-  // for 
-
   console.log(req.body);
-  res.set('content-type','application/json')
+
   res.send("hello");
 
 });
+
+// Initialize the app.
+var server = app.listen(process.env.PORT || 8080, function () {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+  });
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+
 
 
 
